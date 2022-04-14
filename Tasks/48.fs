f3c5261278a507c3c87371a1156f506e027e2f8a
Snow Lukin <everyoursden@gmail.com>
2022-04-15 02:04:39 +0300

    Added task 48

---
+++ Tasks/48.fs
@@ -0,0 +1,41 @@
+﻿module Task48
+
+open System
+
+let private getElement (element, _) = element
+
+let private tupleListToIntList list =
+    let rec loop list resultList =
+        match list with
+        | [] -> resultList
+        | (index, element) :: tail ->
+            loop tail (resultList @ [index])
+    loop list []
+
+
+
+let private getMostFrequentElement list =
+    let newList = list |> List.countBy id
+    getElement (List.maxBy(fun (_, amount) -> amount) newList)
+    
+let private getListOfIndexesOfMostFrequentElement list =
+    let mostFrequentElement = getMostFrequentElement list
+    let indexedList = List.indexed list
+    let filteredList = List.filter (fun (element, _) -> element = mostFrequentElement) indexedList
+    tupleListToIntList filteredList
+
+
+let rec readList numberOfElements =
+    if numberOfElements = 0 then []
+    else
+        let head = Console.ReadLine() |> Convert.ToInt32
+        let tail = readList (numberOfElements - 1)
+        head :: tail
+
+let startTask =
+    //printf "Number of elements in list: "
+    //let listSize = Console.ReadLine() |> Convert.ToInt32
+    //printfn "Input elements"
+    //let list = readList listSize
+    let list = [1; 2; 3; 1; 5; 3; 3]
+    getListOfIndexesOfMostFrequentElement list |> Seq.iter (printf "%d ")
--- Tasks/Program.fs
+++ Tasks/Program.fs
@@ -3,5 +3,6 @@ let main argv =
     //Task8.startTask
     //Task18.startTask
     //Task28.startTask
-    Task38.startTask
+    //Task38.startTask
+    //Task48.startTask
     0
