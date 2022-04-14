8e85727ac2c6378a3d1f9203b7ddbbd78d7bfcf1
Snow Lukin <everyoursden@gmail.com>
2022-04-15 01:15:35 +0300

    Added task 38

---
+++ Tasks/38.fs
@@ -0,0 +1,13 @@
+﻿module Task38
+
+open System
+
+let private amountOfElementsInSegment list startPoint endPoint =
+    let filteredList = List.filter (fun element -> List.contains(element) [startPoint..endPoint]) list
+    filteredList.Length
+
+let startTask =
+    let list = [5; 1; 4; 6; 2]
+    let startPoint = 3
+    let endPoint = 5
+    Console.WriteLine(amountOfElementsInSegment list startPoint endPoint)
--- Tasks/Program.fs
+++ Tasks/Program.fs
@@ -3,4 +3,5 @@ let main argv =
     //Task8.startTask
     //Task18.startTask
     //Task28.startTask
+    Task38.startTask
     0
