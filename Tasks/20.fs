d1986b584effc84e7f664fcf5deecfa980d5e770
Snow Lukin <everyoursden@gmail.com>
2022-05-11 02:17:38 +0300

    Updated task 20.2

--- Tasks/20.fs
+++ Tasks/20.fs
@@ -2,15 +2,20 @@
 
 open System
 
+// convert to ASCII
+let charToInt char = int char - int '0'
+
+let getAsciiAverage (string: string) = 
+    Array.average (Array.map (fun x -> Convert.ToDouble (charToInt x)) (Seq.toArray string))
+
 let private task1 =
     Console.Write("Word: ")
     let string = Console.ReadLine()
-
-    let bytearray : byte[] = Text.Encoding.ASCII.GetBytes(string)
-    let sortedByteArray = Array.sort bytearray
-    let resultString = Text.Encoding.ASCII.GetString(sortedByteArray)
-
-    Console.WriteLine(resultString)
+    // sort by ascii code value to average ascii value in string
+    let temp = (Seq.toArray string) |> Array.sortBy (fun x -> (getAsciiAverage string) - (Convert.ToDouble(charToInt x)))
+    // converting char array to string
+    let result = temp |> Array.map (fun x -> Convert.ToString x) |> String.concat ""
+    Console.WriteLine(result)
 
 let startTask =
     task1
