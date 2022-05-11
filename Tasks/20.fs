54380dfc8573dc86f6edbcf7a0529caed0e94635
Snow Lukin <everyoursden@gmail.com>
2022-05-11 09:51:26 +0300

    Improvements in task20.2

--- Tasks/20.fs
+++ Tasks/20.fs
@@ -3,19 +3,25 @@
 open System
 
 // convert to ASCII
-let charToInt char = int char - int '0'
+let private charToInt char = int char - int '0'
 
-let getAsciiAverage (string: string) = 
+let private getAsciiAverage (string: string) = 
     Array.average (Array.map (fun x -> Convert.ToDouble (charToInt x)) (Seq.toArray string))
 
-let private task1 =
-    Console.Write("Word: ")
-    let string = Console.ReadLine()
+let private task1 string =
     // sort by ascii code value to average ascii value in string
     let temp = (Seq.toArray string) |> Array.sortBy (fun x -> (getAsciiAverage string) - (Convert.ToDouble(charToInt x)))
     // converting char array to string
     let result = temp |> Array.map (fun x -> Convert.ToString x) |> String.concat ""
-    Console.WriteLine(result)
+    result
 
 let startTask =
-    task1
+    printf "Task number: "
+    match Console.ReadLine() |> Convert.ToInt32 with
+    | 1 ->
+        printf "Word: "
+        let string = Console.ReadLine()
+        printf "Result: "
+        Console.WriteLine(task1 string)
+    | 2 -> Console.WriteLine("Under contruction")
+    | _ -> Console.WriteLine("Invalid task number")
