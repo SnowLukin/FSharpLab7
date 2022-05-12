71d10f5bef3220d28962c62a97421e91d6a1e7e6
Snow Lukin <everyoursden@gmail.com>
2022-05-12 20:16:39 +0300

    Improved code

--- Tasks/Lab8_5.fs
+++ Tasks/Lab8_5.fs
@@ -2,22 +2,27 @@
 
 open System
 
-type VehicleLicence(id: int, name: string, model: string, category: string, enginePower: int) =
+type VehicleLicence(id: int, name: string, model: string, category: string, enginePower: int, serialNumber: int, codeNumber: int) =
     member this.id = id
     member this.name = name
     member this.model = model
     member this.category = category
     member this.enginePower = enginePower
+    member this.serialNumber = serialNumber
+    member this.codeNumber = codeNumber
 
     member this.print() =
         Console.WriteLine("Id: {0}", this.id)
         Console.WriteLine("Name: {0}", this.name)
         Console.WriteLine("Model: {0}", this.model)
         Console.WriteLine("Category: {0}", this.category)
-        Console.WriteLine("EnginePower: {0}", enginePower)
+        Console.WriteLine("EnginePower: {0}", this.enginePower)
+        Console.WriteLine("Serial number: {0}", this.serialNumber)
+        Console.WriteLine("Code number: {0}", this.codeNumber)
 
-let startTask =
+let createVehicleLicence =
     printfn "Create Vehicle Licence"
+
     printf "Id: "
     let id = Convert.ToInt32(Console.ReadLine())
     printf "Name: "
@@ -28,6 +33,14 @@ let startTask =
     let category = Console.ReadLine()
     printf "EnginePower: "
     let enginePower = Convert.ToInt32(Console.ReadLine())
+    printf "Serial number: "
+    let serialNumber = Convert.ToInt32(Console.ReadLine())
+    printf "Code number: "
+    let codeNumber = Convert.ToInt32(Console.ReadLine())
+
+    VehicleLicence(id, name, model, category, enginePower, serialNumber, codeNumber)
+
+let startTask =
+    let vehicleLicence = createVehicleLicence
     printfn "Showing results..."
-    let vehicleLicence = VehicleLicence(id, name, model, category, enginePower)
     vehicleLicence.print()
