78f4cbd6fb841375cec78224d16ba12591f5d3b3
Snow Lukin <everyoursden@gmail.com>
2022-05-12 19:53:34 +0300

    Cosmetic fixes

--- Tasks/Lab8/Lab8_5.fs
+++ Tasks/Lab8/Lab8_5.fs
@@ -1,15 +1,9 @@
 ﻿module Lab8_5
 
 type VehicleLicence(id: int, name: string, model: string, category: string, yearOfManufacture: int, enginePower: double) =
-    member this.id
-        with get() = id
-    member this.name
-        with get() = name
-    member this.model
-        with get() = model
-    member this.category
-        with get() = category
-    member this.yearOfManufacture
-        with get() = yearOfManufacture
-    member this.enginePower
-        with get() = enginePower
+    member this.id = id
+    member this.name = name
+    member this.model = model
+    member this.category = category
+    member this.yearOfManufacture = yearOfManufacture
+    member this.enginePower = enginePower
