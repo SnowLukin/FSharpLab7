29b89ce5a710b6c7f7409beee35eaba5dbafda26
Snow Lukin <everyoursden@gmail.com>
2022-05-12 20:40:18 +0300

    Added comparison for class. Cosmetic fixes

--- Tasks/Lab8_5.fs
+++ Tasks/Lab8_5.fs
@@ -2,29 +2,29 @@
 
 open System
 
-type VehicleLicence(id: int, name: string, model: string, category: string, enginePower: int, serialNumber: int, codeNumber: int) =
-    member this.id = id
+type VehicleLicence(serialNumber: int, name: string, model: string, category: string, enginePower: int) =
+    member this.serialNumber = serialNumber
     member this.name = name
     member this.model = model
     member this.category = category
     member this.enginePower = enginePower
-    member this.serialNumber = serialNumber
-    member this.codeNumber = codeNumber
 
     member this.print() =
-        Console.WriteLine("Id: {0}", this.id)
+        Console.WriteLine("Serial number: {0}", this.serialNumber)
         Console.WriteLine("Name: {0}", this.name)
         Console.WriteLine("Model: {0}", this.model)
         Console.WriteLine("Category: {0}", this.category)
         Console.WriteLine("EnginePower: {0}", this.enginePower)
-        Console.WriteLine("Serial number: {0}", this.serialNumber)
-        Console.WriteLine("Code number: {0}", this.codeNumber)
+
+    interface IComparable<VehicleLicence> with
+        member this.CompareTo other =
+            compare this.serialNumber other.serialNumber
 
 let createVehicleLicence =
     printfn "Create Vehicle Licence"
 
-    printf "Id: "
-    let id = Convert.ToInt32(Console.ReadLine())
+    printf "Serial number: "
+    let serialNumber = Convert.ToInt32(Console.ReadLine())
     printf "Name: "
     let name = Console.ReadLine()
     printf "Model: "
@@ -33,12 +33,8 @@ let createVehicleLicence =
     let category = Console.ReadLine()
     printf "EnginePower: "
     let enginePower = Convert.ToInt32(Console.ReadLine())
-    printf "Serial number: "
-    let serialNumber = Convert.ToInt32(Console.ReadLine())
-    printf "Code number: "
-    let codeNumber = Convert.ToInt32(Console.ReadLine())
 
-    VehicleLicence(id, name, model, category, enginePower, serialNumber, codeNumber)
+    VehicleLicence(serialNumber, name, model, category, enginePower)
 
 let startTask =
     let vehicleLicence = createVehicleLicence
