29649b6b8856183dda9f60bc55d2d5e6dd67deca
Snow Lukin <everyoursden@gmail.com>
2022-05-12 19:49:56 +0300

    Added Lab8 task 5 (fixed)

--- Tasks/Lab8/Lab8_5.fs
+++ Tasks/Lab8/Lab8_5.fs
@@ -1,1 +1,15 @@
-﻿
+﻿module Lab8_5
+
+type VehicleLicence(id: int, name: string, model: string, category: string, yearOfManufacture: int, enginePower: double) =
+    member this.id
+        with get() = id
+    member this.name
+        with get() = name
+    member this.model
+        with get() = model
+    member this.category
+        with get() = category
+    member this.yearOfManufacture
+        with get() = yearOfManufacture
+    member this.enginePower
+        with get() = enginePower
