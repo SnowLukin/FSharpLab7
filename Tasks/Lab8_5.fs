229331419e511176598a7479d10c6fd155dd2b1a
Snow Lukin <everyoursden@gmail.com>
2022-05-28 16:59:13 +0300

    Updated lab8_5

--- Tasks/Lab8_5.fs
+++ Tasks/Lab8_5.fs
@@ -71,33 +71,58 @@ type DocumentsArray(documents: array<VehicleLicence>) =
     inherit DocumentCollection()
     member this.documents = documents
     override this.searchDoc(document: VehicleLicence) =
-        Array.exists (fun x -> x = document) this.documents
+        //Array.exists (fun x -> x = document) this.documents
+        if Array.exists (fun x -> x = document) this.documents then
+            printfn "\nArray Found The Document!"
+            (Array.find (fun x -> x = document) this.documents).Print()
+            true
+        else false
 
 type DocumentsList(documents: List<VehicleLicence>) =
     inherit DocumentCollection()
     member this.documents = documents
     override this.searchDoc(document: VehicleLicence) =
-        List.exists (fun x -> x = document) this.documents
+        if List.exists (fun x -> x = document) this.documents then
+            printfn "\nList Found The Document!"
+            (List.find (fun x -> x = document) this.documents).Print()
+            true
+        else false
 
 type DocumentsSet(documents: Set<VehicleLicence>) =
     inherit DocumentCollection()
     member this.documents = documents
     override this.searchDoc(document: VehicleLicence) =
-        Set.exists (fun x -> x = document) this.documents
+        //Set.exists (fun x -> x = document) this.documents
+        if Set.exists (fun x -> x = document) this.documents then
+            printfn "\nSet Found The Document!"
+            let sub = Set.toList this.documents
+            (List.find (fun x -> x = document) sub).Print()
+            true
+        else false
 
+let complexCompare(document: VehicleLicence, document1: VehicleLicence) =
+    if compare document document1 = 1 then 0
+    else if document.serialNumber < document1.serialNumber then 1
+    else 2
+    
 type DocumentsBinaryList(list: List<VehicleLicence>) =
     inherit DocumentCollection()
     member this.list = list |> List.sortBy (fun (document: VehicleLicence) -> document.serialNumber)
     override this.searchDoc(vehicleLicence) =    
         let rec binaryLoop (documentsList: List<VehicleLicence>) (document: VehicleLicence) = 
             match (List.length documentsList) with
-                 |0 -> false
+                 |0 ->
+                    printfn "\nBinary Couldnt Find :("
+                    false
                  |i ->
                     let middle = i / 2
-                    match sign <| compare document documentsList.[middle] with
-                    |0 -> true
-                    |1 -> binaryLoop documentsList.[.. middle - 1] document
-                    |_ -> binaryLoop documentsList.[middle + 1..] document
+                    match complexCompare(document, documentsList.[middle]) with
+                    | 0 ->
+                        printfn "\nBinary Found!"
+                        documentsList.[middle].Print()
+                        true
+                    | 1 -> binaryLoop documentsList.[.. middle - 1] document
+                    | _ -> binaryLoop documentsList.[middle + 1..] document
         binaryLoop this.list vehicleLicence
 
 let measureTime (timer: Stopwatch) method document =
@@ -112,24 +137,28 @@ let compareVehicleLicence v1 v2 =
         printfn "Identical"
     else printfn "Not identical"
 
+let genRandomNumbers count =
+    let rnd = System.Random()
+    List.init count (fun _ -> rnd.Next ())
+
 let startTask =
-    printfn "Create Vehicle Licence"
+    //printfn "Create Vehicle Licence"
 
-    printf "Serial number: "
-    let serialNumber = Convert.ToInt32(Console.ReadLine())
-    printf "Name: "
-    let name = Console.ReadLine()
-    printf "Model: "
-    let model = Console.ReadLine()
-    printf "Category: "
-    let category = Console.ReadLine()
-    printf "EnginePower: "
-    let enginePower = Convert.ToInt32(Console.ReadLine())
+    //printf "Serial number: "
+    //let serialNumber = Convert.ToInt32(Console.ReadLine())
+    //printf "Name: "
+    //let name = Console.ReadLine()
+    //printf "Model: "
+    //let model = Console.ReadLine()
+    //printf "Category: "
+    //let category = Console.ReadLine()
+    //printf "EnginePower: "
+    //let enginePower = Convert.ToInt32(Console.ReadLine())
 
-    let vehicleLicence = VehicleLicence(serialNumber, name, model, category, enginePower)
-    printfn "\nShowing results..."
-    //vehicleLicence.print()
-    vehicleLicence.Print()
+    //let vehicleLicence = VehicleLicence(serialNumber, name, model, category, enginePower)
+    //printfn "\nShowing results..."
+    ////vehicleLicence.print()
+    //vehicleLicence.Print()
 
     // Comparison 
     let v1 = VehicleLicence(1, "", "", "", 1)
@@ -140,14 +169,15 @@ let startTask =
     let timer = new Stopwatch()
 
     let rand = System.Random()
-    let vehicleLicences = List.init(100000) (fun v -> VehicleLicence((rand.Next(100, 100000)), "", "", "", (rand.Next(2000, 20000))))
+    let vehicleLicences = List.init(1000000) (fun v -> VehicleLicence((rand.Next(600, 10000)), "", "", "", (rand.Next(2000, 20000)))) @ [VehicleLicence(500, "FOUND", "FOUND", "FOUND", 1)]
+    
 
     let listDocucuments = DocumentsList(vehicleLicences)
     let arrayDocucuments = DocumentsArray(List.toArray vehicleLicences)
     let setDocDocucuments = DocumentsSet(Set.ofList vehicleLicences)
     let binaryListDocucuments = DocumentsBinaryList(vehicleLicences)
 
-    let missingVehicleLicence = VehicleLicence(500, "", "", "", 1)
+    let missingVehicleLicence = VehicleLicence(500, "FOUND", "FOUND", "FOUND", 1)
 
     printfn "List search time %d ms" (measureTime timer listDocucuments.searchDoc missingVehicleLicence)
     printfn "Array search time %d ms" (measureTime timer arrayDocucuments.searchDoc missingVehicleLicence)
--- Tasks/Program.fs
+++ Tasks/Program.fs
@@ -2,10 +2,11 @@
 let main argv =
     //Lab7Task8.startTask
     //Lab7Task18.startTask
+    //Lab7Task28.startTask
     //Lab7Task38.startTask
     //Lab7Task48.startTask
     //Lab7Task58.startTask
     //Lab7Task17_8.startTask
-    Lab8_OOP.startTask
-    //Lab8_5.startTask
+    //Lab8_OOP.startTask
+    Lab8_5.startTask
     0
